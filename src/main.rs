f05cd17980cf5ce8f560c5ffc663817f004802aa
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-12-24 22:13:46 +0100

    Chapter 3 - exercises

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
 fn main() {
-    let mut number = 3;
-    loop {
-        if number == 0 { break }
-        println!("{number}!");
-        number -= 1;
-    }
-    println!("LIFTOFF!!!");
+    let t1 = temp_converter(Temperature::Celsius, 20.); // -7
+    let t2 = temp_converter(Temperature::Fahrenheit, 10.); // 50
+    println!("T1 is {t1}\n\
+    T2 is {t2}\n");
 
-    let mut number = 3;
-    while number != 0 {
-        println!("{number}!");
-        number -= 1;
-    }
-    println!("LIFTOFF!!!");
+    let result = get_fibonacci(8); //21
+    println!("7th fibonacci number is {result}\n");
 
-    let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
-    while index < 5 {
-        println!("{}", a[index]);
-        index += 1;
-    }
-    for item in a {
-        println!("{item}");
-    }
 
-    for i in 1..=10 {
-        println!("Counting... {i}");
-    }
+}
 
-    for i in (1..=3).rev() {
-        println!("{i}!");
+enum Temperature {
+    Celsius,
+    Fahrenheit
+}
+fn temp_converter(to: Temperature, value: f64) -> f64 {
+    match to {
+        Temperature::Celsius => {
+            5. / 9. * (value - 32.)
+        },
+        Temperature::Fahrenheit => {
+            (value * 9. / 5.) + 32.
+        }
     }
-    println!("LIFTOFF!!!");
+}
+
+fn get_fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 | 2 => 1,
+        num => get_fibonacci(n - 1) + get_fibonacci(n - 2)
+    }
 }
