739be0248b83dc59755737a3919d9f30f5b594f0
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-12-26 08:06:30 +0100

    4 - 3

--- a4_ownership/src/main.rs
+++ a4_ownership/src/main.rs
@@ -1,70 +1,74 @@
 fn main() {
-    let first = String::from("Ferris");
-    let full = add_suffix(first);
+    // fixing returning a reference to the stack
+    let mut s = String::from("");
+    return_a_string(&mut s);
+    println!("{s}");
+
+    // fixing not enough permissions
+    let name = vec![String::from("Adam"), String::from("Peter")];
+    let full = stringify_name_with_title(&name);
     println!("{full}");
-    // println!("{full}, {first}"); - this errors - accessing moved value
 
-    let mut s1 = String::from(":D");
-    println!("{s1}");
-    let s2 = s1;
-    println!("{s2}");
-    s1 = s2;
-    println!("{s1}");
+    // fixing aliasing & mutating a data structure
+    let mut dst = vec![String::from("a"), String::from("bb")];
+    let src = [String::from("ccc")];
+    add_big_strings(&mut dst, &src);
+    println!("{:?}", dst); // ["a", "bb", "ccc"]
 
-    let m1 = String::from("Hello");
-    let m2 = String::from("world");
-    greet(&m1, &m2);
-    println!("{m1}, {m2}");
+    // fixing copying / moving out of a collection
+    let v: Vec<String> = vec![String::from("Hello world")];
+    let s_ref: &String = &v[0];
+    // let s: String = *s_ref; - cannot move
+    println!("{s_ref}!");
 
-    let mut x: Box<i32> = Box::new(1); // x -> 1
-    let a: i32 = *x; // 1
-    *x += 1; // x -> 2
-    println!("{x}, {a}"); // 2, 1
+    // fixing mutating different tuple fields
+    let mut name = (
+        String::from("Ferris"),
+        String::from("Rustacean")
+    );
+    let first = get_first(&name);
+    name.1.push_str(" Esq.");
+    println!("{first}, {}", name.1);
 
-    // r1 is a reference - pointing to stack
-    let r1: &Box<i32> = &x; // reference to x
-    let b: i32 = **r1;
-    println!("{r1}, {b}"); // 2, 2
+    // fixing mutating different array elements
+    let mut a = [0, 1, 2, 3];
+    let x = &mut a[1]; // 1
+    *x += 1;
+    println!("{a:?}"); // [0, 2, 2, 3]
 
-    // r2 is a direct (immutable) reference to the value - pointing to the heap
-    let r2: &i32 = &*x; // reference to the value 2 (value of x)
-    let c: i32 = *r2;
-    println!("{r2}, {c}"); // 2, 2
+    // let y = &a[2];
+     // println!("{x}, {y}"); - errors - immutable & mutable borrows occur at the same time
 
-    // rust implicitly adds references / dereferences in most cases - we must add them explicitly only when calling methods on types, for example
-    let x: Box<i32> = Box::new(-1);
-    let x_abs1 = x.abs();
-    let x_abs2 = i32::abs(*x);
-    assert_eq!(x_abs1, x_abs2);
-
-    let y: &Box<i32> = &x;
-    let y_abs1 = y.abs();
-    let y_abs2 = i32::abs(**y);
-    assert_eq!(y_abs1, y_abs2);
-
-    let z = String::from("Hello");
-    let z_len1 = z.len();
-    let z_len2 = String::len(&z);
-    assert_eq!(z_len1, z_len2);
-
-    let mut v = vec![1, 2, 3];
-    let num = &mut v[2];
-    *num += 1;
-    println!("{num}");
-    println!("{:?}", v); // 1, 2, 4
-
-    let mut x = 1;
-    let y = &x;
-    let z = *y; // 1
-    x += z;
-    println!("{x}");
+    let mut a = [0, 1, 2, 3];
+    let (a_l, a_r) = a.split_at_mut(2);
+    let x = &mut a_l[1];
+    let y = &a_r[0];
+    *x += 10;
+    println!("- {x}\n\
+    - {y}\n");
+    println!("- {a_l:?}\n\
+    - {a_r:?}");
 }
 
-fn add_suffix(mut name: String) -> String {
-    name.push_str(" Jr.");
-    name
+fn return_a_string(s: &mut String)  {
+    s.replace_range(.., "Hello world")
 }
 
-fn greet(g1: &String, g2: &String) {
-    println!("{g1} {g2}!");
+fn stringify_name_with_title(name: &Vec<String>) -> String {
+    let mut full = name.join(" ");
+    full.push_str(" Esq.");
+    full
+}
+
+fn add_big_strings(dst: &mut Vec<String>, src: &[String]) {
+    let largest_len: usize = dst.iter().max_by_key(|s| s.len()).unwrap().len();
+    for s in src {
+        if s.len() > largest_len {
+            dst.push(s.clone());
+        }
+    }
+}
+
+fn get_first(name: &(String, String)) -> String {
+    name.0.clone()
 }
