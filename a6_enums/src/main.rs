0bd3d2d47c23847bbdfea1afdc4d6545cc1087d2
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-12-27 22:04:19 +0100

    6 - 2

--- a6_enums/src/main.rs
+++ a6_enums/src/main.rs
@@ -1,82 +1,130 @@
-#[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
-}
+use rand;
+use rand::Rng;
 
 #[derive(Debug)]
-enum IpAddrKind2 {
-    V4(String),
-    V6(String),
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    NewHampshire,
+    NewJersey,
+    NewMexico,
+    NewYork,
+    NorthCarolina,
+    NorthDakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    RhodeIsland,
+    SouthCarolina,
+    SouthDakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    WashingtonDC,
+    WestVirginia,
+    Wisconsin,
+    Wyoming,
 }
 
-#[derive(Debug)]
-enum IpAddrKind3 {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
-
-#[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
-
-enum Message {
-    Quit, // struct Quit;
-    Move { x: i32, y: i32 }, // struct Move {x: i32, y: i32}
-    // Move (MoveStruct), - same
-    Write(String), // struct Write(String);
-    ChangeColor(i32, i32, i32), // struct ChangeColor(i32, i32, i32);
-}
-
-impl Message {
-    fn call(&self) {
-        () // ...
-    }
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
 }
 
 fn main() {
-    let ip_kind4 = IpAddrKind::V4;
-    let ip_kind6 = IpAddrKind::V6;
-    route(ip_kind4);
-    route(ip_kind6);
+    let my_coin = Coin::Nickel;
+    println!("{}", value_in_cents(&my_coin));
 
-    let ip4 = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
+    let my_coin = Coin::Penny;
+    println!("{}", value_in_cents(&my_coin));
+
+    let my_quarter = Coin::Quarter(UsState::NewYork);
+    println!("{}", value_in_cents(&my_quarter));
+
+    let five = Some(5);
+    let six = plus_one(&five);
+    let none = plus_one(&None);
+    println!("{five:?}, {six:?}, {none:?}");
+
+    let dice_roll = rand::thread_rng().gen_range(1..10);
+    handle_roll(dice_roll);
+
+    // care when binding heap-allocated data to match patterns
+    let opt: Option<String> = Some(String::from("Hello world"));
+    match &opt { // match on a reference, else "s" will be moved
+        Some(s) => println!("Some: {}", s),
+        None => println!("None!")
     };
-    let ip6 = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-    println!("{ip4:?}, {ip6:?}");
-
-    // data inside enums
-    let home = IpAddrKind2::V4(String::from("127.0.0.1"));
-    let loopback = IpAddrKind2::V6(String::from("::1"));
-    println!("{home:?}, {loopback:?}");
-
-    // different data types for enum values
-    let home = IpAddrKind3::V4(127, 0, 0, 1);
-    println!("{home:?}");
-
-    // implementing associated functions / methods on enums
-    let m = Message::Write(String::from("hello"));
-    m.call();
-
-    // Option stdlib enum
-    let some_number = Some(5);
-    let some_char = Some('c');
-    let absent_number: Option<i32> = None;
-    println!("{some_number:?}, {some_char:?}, {absent_number:?}");
-
-    let num1 = 2;
-    let num2 = Some(2);
-    // println!("{}", num1 + num2); - errors, cannot add i32 and Option<i32>
-    println!("{}", num1 + num2.unwrap());
+    println!("{:?}", opt);
 }
 
-fn route(ip_kind: IpAddrKind) {
-    println!("Ip address {ip_kind:?}");
+fn value_in_cents(coin: &Coin) -> i32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
 }
+
+fn plus_one(num: &Option<i32>) -> Option<i32> {
+    match num {
+        Some(n) => Some(n + 1),
+        None => None
+    }
+}
+
+fn handle_roll(roll: i32) {
+    println!("You rolled {roll}!");
+    match roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        1 | 2 | 4 | 5 => move_player(roll),
+        _ => println!("You must roll again!")
+        // _ => () - also possible - nothing happens
+    }
+}
+
+fn add_fancy_hat() { println!("You got a fancy hat!"); }
+
+fn remove_fancy_hat() { println!("Oh no, you lost your fancy hat!"); }
+
+fn move_player(spaces: i32) { println!("You moved {spaces} spaces."); }
