31d3ac902c61c1b93125ea567b41c0c9a823986b
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-12-27 22:35:57 +0100

    6 - 3, 4

--- a6_enums/src/main.rs
+++ a6_enums/src/main.rs
@@ -1,6 +1,3 @@
-use rand;
-use rand::Rng;
-
 #[derive(Debug)]
 enum UsState {
     Alabama,
@@ -56,6 +53,7 @@ enum UsState {
     Wyoming,
 }
 
+#[derive(Debug)]
 enum Coin {
     Penny,
     Nickel,
@@ -64,67 +62,24 @@ enum Coin {
 }
 
 fn main() {
-    let my_coin = Coin::Nickel;
-    println!("{}", value_in_cents(&my_coin));
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {max}"),
+        _ => ()
+    }
+    if let Some(max) = config_max { // same thing
+        println!("The maximum is configured to be {max}");
+    }
 
-    let my_coin = Coin::Penny;
-    println!("{}", value_in_cents(&my_coin));
-
-    let my_quarter = Coin::Quarter(UsState::NewYork);
-    println!("{}", value_in_cents(&my_quarter));
-
-    let five = Some(5);
-    let six = plus_one(&five);
-    let none = plus_one(&None);
-    println!("{five:?}, {six:?}, {none:?}");
-
-    let dice_roll = rand::thread_rng().gen_range(1..10);
-    handle_roll(dice_roll);
-
-    // care when binding heap-allocated data to match patterns
-    let opt: Option<String> = Some(String::from("Hello world"));
-    match &opt { // match on a reference, else "s" will be moved
-        Some(s) => println!("Some: {}", s),
-        None => println!("None!")
-    };
-    println!("{:?}", opt);
-}
-
-fn value_in_cents(coin: &Coin) -> i32 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {state:?}!");
-            25
-        }
+    let mut count = 0;
+    let my_coin = Coin::Quarter(UsState::NewYork);
+    match &my_coin {
+        Coin::Quarter(state) => println!("State quarter from {state:?}!"),
+        _ => count += 1
+    }
+    if let Coin::Quarter(state) = &my_coin { // same thing
+        println!("State quarter from {state:?}!");
+    } else { // we can also include code for the catch-all part
+        count += 1
     }
 }
-
-fn plus_one(num: &Option<i32>) -> Option<i32> {
-    match num {
-        Some(n) => Some(n + 1),
-        None => None
-    }
-}
-
-fn handle_roll(roll: i32) {
-    println!("You rolled {roll}!");
-    match roll {
-        3 => add_fancy_hat(),
-        7 => remove_fancy_hat(),
-        1 | 2 | 4 | 5 => move_player(roll),
-        _ => println!("You must roll again!")
-        // _ => () - also possible - nothing happens
-    }
-}
-
-fn add_fancy_hat() { println!("You got a fancy hat!"); }
-
-fn remove_fancy_hat() { println!("Oh no, you lost your fancy hat!"); }
-
-fn move_player(spaces: i32) { println!("You moved {spaces} spaces."); }
