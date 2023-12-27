7bdd247c73a0ec4dd9d7c09c7b68206c0234bdea
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-12-27 05:51:08 +0100

    4 - 4

--- a4_ownership/src/main.rs
+++ a4_ownership/src/main.rs
@@ -1,74 +1,58 @@
 fn main() {
-    // fixing returning a reference to the stack
-    let mut s = String::from("");
-    return_a_string(&mut s);
+    let s = String::from("hello world");
+    let hello: &str = &s[..5];
+    let world: &str = &s[6..];
+    let s2: &String = &s; // String reference, not a slice
+    println!("{hello}, {world}, {s2}");
+
+    let s = String::from("hello");
+    let slice1 = &s[0..2];
+    let slice2 = &s[..2];
+    assert_eq!(slice1, slice2);
+
+    let slice1 = &s[3..s.len()];
+    let slice2 = &s[3..];
+    assert_eq!(slice1, slice2);
+
+    let slice1 = &s[0..s.len()];
+    let slice2 = &s[..];
+    assert_eq!(slice1, slice2);
+
+    // string literals are slices
+    let s = "Hello, world!"; // s is an immutable reference to the binary
     println!("{s}");
 
-    // fixing not enough permissions
-    let name = vec![String::from("Adam"), String::from("Peter")];
-    let full = stringify_name_with_title(&name);
-    println!("{full}");
+    // using first_word with its &str parameter
+    let my_string = String::from("hello world");
+    let word1 = first_word(&my_string[0..6]);
+    let word2 = first_word(&my_string[..]);
+    let word3 = first_word(&my_string);
+    assert_eq!((word1, word2, word3), ("hello", "hello", "hello"));
 
-    // fixing aliasing & mutating a data structure
-    let mut dst = vec![String::from("a"), String::from("bb")];
-    let src = [String::from("ccc")];
-    add_big_strings(&mut dst, &src);
-    println!("{:?}", dst); // ["a", "bb", "ccc"]
+    let my_string_literal = "hello world";
+    let word1 = first_word(&my_string_literal[0..6]);
+    let word2 = first_word(&my_string_literal[..]);
+    let word3 = first_word(my_string_literal); // works on the string slice itself
+    assert_eq!((word1, word2, word3), ("hello", "hello", "hello"));
 
-    // fixing copying / moving out of a collection
-    let v: Vec<String> = vec![String::from("Hello world")];
-    let s_ref: &String = &v[0];
-    // let s: String = *s_ref; - cannot move
-    println!("{s_ref}!");
+    // other slices
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
 
-    // fixing mutating different tuple fields
-    let mut name = (
-        String::from("Ferris"),
-        String::from("Rustacean")
+    println!(
+        "&String={} &str={}",
+        std::mem::size_of::<&String>(), // &String contains just the pointer to the stack (reference)
+        std::mem::size_of::<&str>(), // &str contains the direct pointer to the heap and the size
     );
-    let first = get_first(&name);
-    name.1.push_str(" Esq.");
-    println!("{first}, {}", name.1);
-
-    // fixing mutating different array elements
-    let mut a = [0, 1, 2, 3];
-    let x = &mut a[1]; // 1
-    *x += 1;
-    println!("{a:?}"); // [0, 2, 2, 3]
-
-    // let y = &a[2];
-     // println!("{x}, {y}"); - errors - immutable & mutable borrows occur at the same time
-
-    let mut a = [0, 1, 2, 3];
-    let (a_l, a_r) = a.split_at_mut(2);
-    let x = &mut a_l[1];
-    let y = &a_r[0];
-    *x += 10;
-    println!("- {x}\n\
-    - {y}\n");
-    println!("- {a_l:?}\n\
-    - {a_r:?}");
 }
 
-fn return_a_string(s: &mut String)  {
-    s.replace_range(.., "Hello world")
-}
-
-fn stringify_name_with_title(name: &Vec<String>) -> String {
-    let mut full = name.join(" ");
-    full.push_str(" Esq.");
-    full
-}
-
-fn add_big_strings(dst: &mut Vec<String>, src: &[String]) {
-    let largest_len: usize = dst.iter().max_by_key(|s| s.len()).unwrap().len();
-    for s in src {
-        if s.len() > largest_len {
-            dst.push(s.clone());
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
         }
     }
-}
-
-fn get_first(name: &(String, String)) -> String {
-    name.0.clone()
+    &s[..]
 }
