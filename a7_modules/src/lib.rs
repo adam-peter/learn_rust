2422594daee5fd88b7e0238c6d970df38c5ad20a
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-12-29 11:15:46 +0100

    7 - 4

--- a7_modules/src/lib.rs
+++ a7_modules/src/lib.rs
@@ -4,51 +4,21 @@ mod front_of_house {
     }
 }
 
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-
-    #[derive(Debug)]
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String
-    }
-
-    // since all of the fields aren't public - we need a pub associated function on Breakfast to construct it (new, here it's summer)
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches")
-            }
-        }
-    }
-
-    // if enum is pub - all of enum variants are pub
-    pub enum Appetizer {
-        Soup,
-        Salad
-    }
-}
-
-fn deliver_order() {}
+// external code before: restaurant::front_of_house::hosting::add_to_waitlist();
+pub use crate::front_of_house::hosting; // external code after: restaurant::hosting::add_to_waitlist();
 
 pub fn eat_at_restaurant() {
-    // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-    // relative path
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
 
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please.", meal.toast);
-    println!("{meal:?}");
-    // meal.seasonal_fruit = String::from("blueberries"); - error - seasonal_fruit field is private
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+fn function1() -> Result {
+    todo!()
+}
+
+fn function2() -> IoResult<()> {
+    todo!()
 }
