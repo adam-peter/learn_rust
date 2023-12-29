bb5d6619c84bd1c92681a722d95262fbb25f4958
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-12-29 11:24:58 +0100

    7 - 5

--- a7_modules/src/lib.rs
+++ a7_modules/src/lib.rs
@@ -1,24 +1,7 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+mod front_of_house;
 
-// external code before: restaurant::front_of_house::hosting::add_to_waitlist();
-pub use crate::front_of_house::hosting; // external code after: restaurant::hosting::add_to_waitlist();
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
-}
-
-
-use std::fmt::Result;
-use std::io::Result as IoResult;
-
-fn function1() -> Result {
-    todo!()
-}
-
-fn function2() -> IoResult<()> {
-    todo!()
 }
