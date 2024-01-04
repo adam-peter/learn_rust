7f7c6250ee7584e6ba436caeed3afe81301b6f18
adam-peter <petrzela.adam@seznam.cz>
2024-01-04 05:24:33 +0100

    10 - 1

--- a10_generics/src/main.rs
+++ a10_generics/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+    let mut num = &number_list[0];
+    for number in &number_list {
+        if number > num {
+            num = number
+        }
+    }
+    println!("The largest number is {num}");
+
+    // extracting a function to reduce duplication
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let num = largest(&number_list);
+    println!("The largest number is {}", num);
+}
+
+fn largest(number_list: &[i32]) -> &i32 {
+    let mut largest = &number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
 }
