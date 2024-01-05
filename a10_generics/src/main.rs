23e9cf653e42f3eb8a777f38f7a66a82df202088
adam-peter <petrzela.adam@seznam.cz>
2024-01-05 06:03:52 +0100

    10 - 2

--- a10_generics/src/main.rs
+++ a10_generics/src/main.rs
@@ -1,4 +1,4 @@
-use a10_generics::{Summary, Tweet};
+use a10_generics::{NewsArticle, notify, Summary, Tweet};
 
 fn main() {
     let tweet = Tweet {
@@ -8,4 +8,17 @@ fn main() {
         retweet: false
     };
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+            hockey team in the NHL."
+        )
+    };
+    println!("New article available! {}", article.summarize());
+
+    notify(&article);
 }
