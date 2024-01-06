e1097e39d5ec9d84af5885c16f859d0eac4a6d3d
adam-peter <petrzela.adam@seznam.cz>
2024-01-06 20:23:41 +0100

    11 - 1 (1/2)

---
+++ a11_testing/src/lib.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+}
