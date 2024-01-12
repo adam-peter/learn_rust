7bed4a7ee1df5b1be39cc55a60d0c381c3446581
adam-peter <petrzela.adam@seznam.cz>
2024-01-12 18:23:12 +0100

    12 - 1

--- a12_minigrep/src/main.rs
+++ a12_minigrep/src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for \"{query}\" in \"{file_path}\"");
 }
