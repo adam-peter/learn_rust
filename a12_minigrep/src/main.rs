927396df1f0926849c315ce461eb37ae8baf1d6b
adam-peter <petrzela.adam@seznam.cz>
2024-01-12 18:32:42 +0100

    12 - 2

--- a12_minigrep/src/main.rs
+++ a12_minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,4 +6,8 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
     println!("Searching for \"{query}\" in \"{file_path}\"");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Cannot read the file");
+    println!("Contents:\n{contents}");
 }
