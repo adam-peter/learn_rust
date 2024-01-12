ac95f7d0b259d1cc695688a422ef835a240f1884
adam-peter <petrzela.adam@seznam.cz>
2024-01-12 06:43:16 +0100

    11 - 1

--- a11_testing/src/lib.rs
+++ a11_testing/src/lib.rs
@@ -4,10 +4,34 @@ mod tests;
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
-    height: u32
+    height: u32,
 }
+
 impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width >= other.width && self.height >= other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
+        }
+        Guess { value }
     }
 }
--- a11_testing/src/tests/mod.rs
+++ a11_testing/src/tests/mod.rs
@@ -1,6 +1,12 @@
 use crate::*;
 
 #[test]
+fn it_works() {
+    let result = 1 + 1;
+    assert_eq!(result, 2);
+}
+
+#[test]
 fn exploration() {
     assert_eq!(2 + 2, 4);
 }
@@ -37,4 +43,37 @@ fn smaller_cannot_hold_larger() {
     };
 
     assert!(!smaller.can_hold(&larger));
+}
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(add_two(2), 4);
+}
+
+#[test]
+fn greeting_contains_name() {
+    let result = greeting("Adam");
+    // passing arguments after assertion passes them into format!() macro
+    assert!(result.contains("Adam"),
+            "Greeting didn't contain name, value was \"{}\"",
+            result);
+}
+
+#[test]
+#[should_panic(expected = "less than or equal to 100")]
+fn greater_than_100() {
+    // Guess::new(-1); - panics for a different reason, than we expect in the expected parameter
+    Guess::new(200);
+}
+
+// tests can return Results and error on returning Error
+#[test]
+fn it_works_with_result() -> Result<(), String> {
+    // when returning result, we can use the "?" operator inside the test fn
+    if 2 + 2 == 4 {
+        Ok(())
+    } else {
+        Err(String::from("two plus two didn't equal four"))
+    }
+    // we cannot use the #[should_panic] annotation, instead, use assert!(value.is_err())
 }
