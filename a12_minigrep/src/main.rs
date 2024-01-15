d031fe7e8714be10f57e0c7cb2058f948c62b0dd
adam-peter <petrzela.adam@seznam.cz>
2024-01-15 05:41:12 +0100

    12 - 3

--- a12_minigrep/src/main.rs
+++ a12_minigrep/src/main.rs
@@ -1,13 +1,16 @@
-use std::{env, fs};
+use std::{env, process};
+use a12_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("Searching for \"{}\" in \"{}\"", config.query, config.file_path);
 
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for \"{query}\" in \"{file_path}\"");
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Cannot read the file");
-    println!("Contents:\n{contents}");
+    if let Err(e) = a12_minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
