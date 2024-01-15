723541efbfad608e6d2fc76999e1c751f158793b
adam-peter <petrzela.adam@seznam.cz>
2024-01-15 06:35:35 +0100

    12 - 5

--- a12_minigrep/src/lib.rs
+++ a12_minigrep/src/lib.rs
@@ -2,11 +2,12 @@
 mod tests;
 
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -17,14 +18,30 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let mut ignore_case = env::var("IGNORE_CASE").is_ok(); // var returns result with val, we only care that it's ok, not the value
+        if let Some(option) = args.get(3) { // processing ignore case if it's specified as a command line argument (takes precedence over env vars)
+            let option: i32 = option.parse().unwrap();
+            if option == 1 {
+                ignore_case = true;
+            } else {
+                ignore_case = false;
+            }
+        }
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -36,6 +53,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
--- a12_minigrep/src/tests/mod.rs
+++ a12_minigrep/src/tests/mod.rs
@@ -1,12 +1,25 @@
 use crate::*;
 
 #[test]
-fn one_result() {
+fn case_sensitive() {
     let query = "duct";
     let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape."; // this line won't be in the test result (case sensitive)
 
     assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+}
+
+#[test]
+fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
 }
